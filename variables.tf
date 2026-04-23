92c3756158275b03c3dc839a0d00f866e6a31bbc
penubadi Haritha <harithapenubadi.com>
2026-04-23 12:26:08 +0530

    m2

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "location" {
+  default = "West Us"
+}
+
+variable "resource_group_name" {
+  default = "prameela-rg"
+}
+
+variable "admin_username" {
+  default = "haritha"
+}
+
+variable "admin_password" {
+  default = "haritha123!"
+}
+
+variable "mysql_admin" {
+  default = "ordinary"
+}
+
+variable "mysql_password" {
+  default = "StrongPasss#123"
+}
+
